1c43c41839fa0f875050f25afe61a786bf94fe47
jirhiker <jirhiker@users.noreply.github.com>
2022-11-19 05:54:13 +0000

    Formatting changes

--- api/routes/wl.py
+++ api/routes/wl.py
@@ -38,7 +38,7 @@ router = APIRouter()
     tags=["Groundwater Levels"],
 )
 def read_waterlevels(
-        point_id: str = None, limit: int = 1000, db: Session = Depends(get_waterdb)
+    point_id: str = None, limit: int = 1000, db: Session = Depends(get_waterdb)
 ):
     fs = [ObservedProperty.name == "DepthToWaterBGS"]
     js = [ObservedProperty]
@@ -64,7 +64,7 @@ def read_waterlevels(
     tags=["Groundwater Temperatures"],
 )
 def read_temperatures(
-        point_id: str = None, limit: int = 1000, db: Session = Depends(get_waterdb)
+    point_id: str = None, limit: int = 1000, db: Session = Depends(get_waterdb)
 ):
     fs = [ObservedProperty.name == "WellTemperature"]
     js = [ObservedProperty]
@@ -111,10 +111,10 @@ def read_temperatures(
 #
 @router.get("/locations", response_model=Page[wl_schemas.Location], tags=["Locations"])
 def read_locations(
-        point_id: str = None,
-        public_release: bool = None,
-        db: Session = Depends(get_waterdb),
-        params: Params = Depends(),
+    point_id: str = None,
+    public_release: bool = None,
+    db: Session = Depends(get_waterdb),
+    params: Params = Depends(),
 ):
     filters = []
     if point_id:
@@ -128,10 +128,10 @@ def read_locations(
 
 @router.get("/wells", response_model=Page[wl_schemas.Well], tags=["Wells"])
 def read_wells(
-        location_id: int = None,
-        point_id: str = None,
-        db: Session = Depends(get_waterdb),
-        params: Params = Depends(),
+    location_id: int = None,
+    point_id: str = None,
+    db: Session = Depends(get_waterdb),
+    params: Params = Depends(),
 ):
     joins = []
     filters = []
@@ -142,5 +142,6 @@ def read_wells(
         filters.append(Well.location_id == location_id)
 
     return paginate(_read(db, Well, joins=joins, filters=filters), params)
+
 
 # ============= EOF =============================================
