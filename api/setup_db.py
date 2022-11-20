fbb2206a99a5c2a032b3829c8d4f1fe3a7d4c1bf
jirhiker <jirhiker@users.noreply.github.com>
2022-11-20 15:20:19 +0000

    Formatting changes

--- api/setup_db.py
+++ api/setup_db.py
@@ -221,7 +221,9 @@ def copy_gw_locations(cursor, dest, obsprop_bgs, locations):
             copy_gw_location(projection, cursor, dest, obsprop_bgs, l)
         except BaseException:
             failures.append(l)
-        printProgressBar(i, total, prefix=f'Sync PointID={l["PointID"]}', suffix='Complete')
+        printProgressBar(
+            i, total, prefix=f'Sync PointID={l["PointID"]}', suffix="Complete"
+        )
 
     return failures
 
@@ -268,8 +270,18 @@ def copy_nm_aquifer(dest):
     dest.commit()
     src.close()
 
+
 # Print iterations progress
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+def printProgressBar(
+    iteration,
+    total,
+    prefix="",
+    suffix="",
+    decimals=1,
+    length=100,
+    fill="█",
+    printEnd="\r",
+):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -284,10 +296,11 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    bar = fill * filledLength + "-" * (length - filledLength)
+    print(f"\r{prefix} |{bar}| {percent}% {suffix}", end=printEnd)
     # Print New Line on Complete
     if iteration == total:
         print()
+
 
 # ============= EOF =============================================
